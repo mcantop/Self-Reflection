844c5336b09d902e8ead8ba07dfdf1e9ed5ac449
mcantop <maciejcantop@icloud.com>
2022-08-31 22:40:18 +0200

    Initial Commit

---
+++ Self Reflection/Self_ReflectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Self_ReflectionApp.swift
+//  Self Reflection
+//
+//  Created by Maciej on 31/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Self_ReflectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
